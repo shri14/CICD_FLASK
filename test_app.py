71074adffaf6722e8de952c25871bfea11ae13b5
Devopsbuddy <98806710+shri14@users.noreply.github.com>
2024-02-11 11:00:27 +0530

    Create test_app.py

---
+++ test_app.py
@@ -0,0 +1,15 @@
+import unittest
+from app import app
+
+class TestApp(unittest.TestCase):
+
+    def setUp(self):
+        self.app = app.test_client()
+
+    def test_hello_world(self):
+        response = self.app.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data.decode('utf-8'), 'Hello, World!')
+
+if __name__ == '__main__':
+    unittest.main()
